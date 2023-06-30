4b136338572d67dbe9d5edf2a8fdb12bb7dda99a
Kamyar Ghazanfari <ghazanfarik79@gmail.com>
2023-06-30 13:31:09 +0330

    jwt configs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as passport from 'passport';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'secret',
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }

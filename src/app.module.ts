6e2a1c930eedd39c0dbdbcfc74e879207aae368a
Kamyar Ghazanfari <ghazanfarik79@gmail.com>
2023-06-30 16:04:30 +0330

    Add i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+} from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        new HeaderResolver(['x-custom-lang']),
+        new AcceptLanguageResolver(),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
